5c8a3656a650f077c40096e50d9ebef77f30185e
dosoft <dmmotec@gmail.com>
2016-12-09 16:33:45 +0100

    Add project files.

---
+++ FacebookPostOnPage/FacebookAPI.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using Facebook;
+using Newtonsoft.Json.Linq;
+
+namespace FacebookPostOnPage
+{
+    public class FacebookApi
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="FacebookApi" /> class.
+        /// </summary>
+        /// <param name="appId">The application identifier.</param>
+        /// <param name="appSecret">The application secret.</param>
+        public FacebookApi(string appId, string appSecret)
+        {
+            ResponseJsonObject = new JsonObject();
+            AppId = appId;
+            AppSecret = appSecret;
+        }
+
+        public string AppId { get; }
+
+        public string AppSecret { get; }
+
+        public JsonObject ResponseJsonObject { get; set; }
+
+
+        /// <summary>
+        ///     Gets the page access token.
+        /// </summary>
+        /// <param name="userAccessToken">The user access token.</param>
+        /// <param name="pageName">Name of the page.</param>
+        /// <returns></returns>
+        public string GetPageAccessToken(string userAccessToken, string pageName)
+        {
+            var fbClient = new FacebookClient();
+            fbClient.AppId = AppId;
+            fbClient.AppSecret = AppSecret;
+            fbClient.AccessToken = userAccessToken;
+            var fbParams = new Dictionary<string, object>();
+            var publishedResponse = fbClient.Get("/me/accounts", fbParams) as JsonObject;
+            var data = JArray.Parse(publishedResponse["data"].ToString());
+
+            foreach (var account in data)
+                if (account["name"].ToString().ToLower().Equals(pageName))
+                    return account["access_token"].ToString();
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        ///     Refreshes the user access token.
+        /// </summary>
+        /// <param name="currentAccessToken">The current access token.</param>
+        /// <returns></returns>
+        public string RefreshUserAccessToken(string currentAccessToken)
+        {
+            var fbClient = new FacebookClient();
+            var fbParams = new Dictionary<string, object>();
+            fbParams["client_id"] = AppId; // app ID https://developers.facebook.com/apps/947691342016439/dashboard/
+            fbParams["grant_type"] = "fb_exchange_token";
+            fbParams["client_secret"] = AppSecret;
+            fbParams["fb_exchange_token"] = currentAccessToken;
+            var publishedResponse = fbClient.Get("/oauth/access_token", fbParams) as JsonObject;
+            return publishedResponse["access_token"].ToString();
+        }
+
+        /// <summary>
+        /// Posts to facebook.
+        /// </summary>
+        /// <param name="pageAccessToken">The page access token.</param>
+        /// <param name="pageid">The pageid.</param>
+        /// <param name="facebookParameters">The facebook parameters.</param>
+        public void PostToFacebook(string pageAccessToken, string pageid, Dictionary<string, object> facebookParameters)
+        {
+            var fbClient = new FacebookClient(pageAccessToken);
+            fbClient.AppId = AppId;
+            fbClient.AppSecret = AppSecret;
+            // https://developers.facebook.com/docs/graph-api/reference/v2.6/post
+            //Dictionary<string, object> fbParams = new Dictionary<string, object>();
+            //fbParams["message"] = message;
+            //fbParams["link"] =
+            //fbParams["picture"] =
+            //fbParams["name"] =
+            //fbParams["caption"] =
+            //fbParams["description"] =
+            var publishedResponse = fbClient.Post($"/{pageid}/feed", facebookParameters);
+            if (publishedResponse != null)
+                if (typeof(JsonObject) == publishedResponse.GetType())
+                {
+                    var respJsonObject = (JsonObject) publishedResponse;
+                    ResponseJsonObject = respJsonObject;
+                }
+        }
+
+        /// <summary>
+        /// Deletes the facebook post.
+        /// </summary>
+        /// <param name="pageAccessToken">The page access token.</param>
+        /// <param name="pageid">The pageid.</param>
+        /// <param name="facebookParameters">The facebook parameters.</param>
+        public void DeleteFacebookPost(string pageAccessToken, string pageid,
+            Dictionary<string, object> facebookParameters)
+        {
+            var fbClient = new FacebookClient(pageAccessToken);
+            fbClient.AppId = AppId;
+            fbClient.AppSecret = AppSecret;
+
+            var publishedResponse = fbClient.Delete($"/{pageid}", facebookParameters);
+            if (publishedResponse != null)
+                if (typeof(JsonObject) == publishedResponse.GetType())
+                {
+                    var respJsonObject = (JsonObject) publishedResponse;
+                    ResponseJsonObject = respJsonObject;
+                }
+        }
+
+        /// <summary>
+        /// Posts to facebook.
+        /// </summary>
+        /// <param name="pageAccessToken">The page access token.</param>
+        /// <param name="pageid">The pageid.</param>
+        /// <param name="message">The message.</param>
+        public void PostToFacebook(string pageAccessToken, string pageid, string message)
+        {
+            var fbParamters = new Dictionary<string, object>();
+            fbParamters["message"] = message;
+            PostToFacebook(pageAccessToken, pageid, fbParamters);
+        }
+
+        /// <summary>
+        /// Deletes the facebook post.
+        /// </summary>
+        /// <param name="pageAccessToken">The page access token.</param>
+        /// <param name="pageid">The pageid.</param>
+        public void DeleteFacebookPost(string pageAccessToken, string pageid)
+        {
+            var fbParamters = new Dictionary<string, object>();
+            DeleteFacebookPost(pageAccessToken, pageid, fbParamters);
+        }
+    }
+}
---
+++ FacebookPostOnPage/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+
+namespace FacebookPostOnPage
+{
+    internal class Program
+    {
+        private const string filename = "facebookaccess.txt";
+
+        private static void Main(string[] args)
+        {
+            // Create Facebook app for the appId, appSecret https://developers.facebook.com/apps
+            var facebookApi = new FacebookApi("XXXX", "YYYYYY");
+            var isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User |
+                                                        IsolatedStorageScope.Assembly, null, null);
+            var curUserToken = "";
+            try
+            {
+                curUserToken = ReadFromFile(isoStore);
+            }
+            catch (Exception)
+            {
+                // DO something
+                //throw;
+            }
+
+            // get default user token for your project https://developers.facebook.com/tools/accesstoken/
+            if (string.IsNullOrEmpty(curUserToken))
+                curUserToken =
+                    "ZZZZd65XML7cBAAOrobEwDtHCSjyINIz1sglWNV75KGDBTOGsnoJKGwQWpn4UBUGRk4EYNAZBc92qMFxIeliycZCFydXRwywYMrCei6CUkJZAVRfRT3yXEZAN6D2SUaYFe0qsK6l1vuWm0eWZAQjWr1OjDYSKVJUCNYxX3QOJVhwZDZD";
+
+            try
+            {
+                // get long-lived tokens (60days)
+                var newUserAccessToken = facebookApi.RefreshUserAccessToken(curUserToken);
+
+                // Save the new token 
+                WriteToFile(isoStore, newUserAccessToken);
+
+                // get page access token
+                var pageAccess = facebookApi.GetPageAccessToken(newUserAccessToken, "dmmosoftware");
+                if (string.IsNullOrEmpty(pageAccess))
+                {
+                    Console.Write("ERROR: No user access token!");
+                    // TODO - ERROR handling
+                }
+                else
+                {
+                    var message = "Testnachricht: " + RandomString(20);
+                    facebookApi.PostToFacebook(pageAccess, "dmmosoftware", message);
+
+                    var id = facebookApi.ResponseJsonObject["id"];
+                    Console.WriteLine("PostToFacebook new id: " + id);
+
+                    facebookApi.DeleteFacebookPost(pageAccess, id.ToString());
+
+                    var success = facebookApi.ResponseJsonObject["success"];
+                    Console.WriteLine("Delete post: " + success);
+                }
+            }
+            catch (Exception exception)
+            {
+                Trace.Write(exception);
+                throw;
+            }
+            Console.ReadKey();
+        }
+
+        #region helper
+
+        /// <summary>
+        ///     Writes to file.
+        /// </summary>
+        /// <param name="isoStore">The iso store.</param>
+        /// <param name="curUserAccess">The current user access.</param>
+        private static void WriteToFile(IsolatedStorageFile isoStore, string curUserAccess)
+        {
+            StreamWriter writer = null;
+
+            writer = new StreamWriter(new IsolatedStorageFileStream(
+                filename, FileMode.Create, isoStore));
+
+            writer.WriteLine(curUserAccess);
+
+            writer.Close();
+        }
+
+        /// <summary>
+        ///     Reads from file.
+        /// </summary>
+        /// <param name="isoStore">The iso store.</param>
+        /// <returns></returns>
+        private static string ReadFromFile(IsolatedStorageFile isoStore)
+        {
+            var reader = new StreamReader(new IsolatedStorageFileStream(
+                filename, FileMode.Open, isoStore));
+
+            var sb = reader.ReadLine();
+            reader.Close();
+
+            return sb;
+        }
+
+        /// <summary>
+        ///     Randoms the string.
+        /// </summary>
+        /// <param name="length">The length.</param>
+        /// <returns></returns>
+        private static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHDTZHDIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        #endregion
+    }
+}
